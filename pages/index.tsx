eca3c259d9003f3ad197378e4820a4b43cce8422
Abeltony-prog <abeltony03@gmail.com>
2025-04-12 08:21:54 +0200

    Initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
+import AllItems from "@components/ui/items/items";
 import RootLayout from "@components/ui/layout";
 import SideBar from "@components/ui/sidebar";
 
 export default function Home() {
   return (
     <RootLayout>
-      <SideBar />
+     <AllItems />
     </RootLayout>
   );
 }
