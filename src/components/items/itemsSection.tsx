a7e6435a1772c1e61fcfd947916174b4cd22fc21
Abeltony-prog <abeltony03@gmail.com>
2025-04-13 19:04:41 +0200

    Initial commit

---
+++ src/components/items/itemsSection.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import ProdCategories from "@components/ui/categories";
+import Products from "./Products";
+
+export default function ItemsSection() {
+  return (
+   <>
+           <ProdCategories />
+<Products />
+   </>
+ 
+
+   
+  );
+}
+
+
