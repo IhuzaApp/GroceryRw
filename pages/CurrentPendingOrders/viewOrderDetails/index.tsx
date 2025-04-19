e587e50344b0efafd048aae4c0adeb3e724047e9
Abeltony-prog <abeltony03@gmail.com>
2025-04-19 21:36:57 +0200

    Initial commit

---
+++ pages/CurrentPendingOrders/viewOrderDetails/index.tsx
@@ -0,0 +1,18 @@
+import UserPendingOrders from "@components/UserCarts/orders/UserPendingOrders";
+import RootLayout from "@components/ui/layout";
+import Link from "next/link";
+import React from "react";
+
+export default function ViewOrderDetailsPage(){
+    return(
+        <RootLayout>
+                <div className="p-4 md:ml-16">
+        {/* Adjust ml-* to match your sidebar width */}
+        <div className="container mx-auto">
+       
+            <UserPendingOrders />
+            </div>
+            </div>
+        </RootLayout>
+    )
+}
