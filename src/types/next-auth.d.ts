cdf3e8ef356b4f550a93a9cf94212bb027013335
Abeltony-prog <abeltony03@gmail.com>
2025-04-29 02:30:15 +0200

    login and registerr

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,21 @@
+import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+
+// Extend the default session interface with an `id` on user
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+
+  interface User extends DefaultUser {
+    id: string;
+  }
+}
+
+// Extend the JWT payload to include user id
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+  }
+} 
