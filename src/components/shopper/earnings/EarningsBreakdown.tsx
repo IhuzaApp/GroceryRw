1d1277fe384d8b16256cb7f8eb91e453b294c034
Abeltony-prog <abeltony03@gmail.com>
2025-05-14 05:51:11 +0200

    earning goals

--- src/components/shopper/earnings/EarningsBreakdown.tsx
+++ src/components/shopper/earnings/EarningsBreakdown.tsx
@@ -24,9 +24,10 @@ const EarningsBreakdown: React.FC<EarningsBreakdownProps> = ({
 }) => {
   // Format currency
   const formatCurrency = (amount: number) => {
-    return new Intl.NumberFormat('en-US', {
+    return new Intl.NumberFormat('en-RW', {
       style: 'currency',
-      currency: 'USD',
+      currency: 'RWF',
+      maximumFractionDigits: 0
     }).format(amount);
   };
 
