82ee4db57bfa58a06237642d978ef668b31d71a6
Abeltony-prog <abeltony03@gmail.com>
2025-06-25 04:03:28 +0200

    fix on the css

--- src/components/ui/NavBar/bottomBar.tsx
+++ src/components/ui/NavBar/bottomBar.tsx
@@ -498,7 +498,7 @@ export default function BottomBar() {
                     id="Page-1"
                     stroke="none"
                     strokeWidth="1"
-                    fill="none"
+                    fill="currentColor"
                     fillRule="evenodd"
                   >
                     {" "}
@@ -548,7 +548,7 @@ export default function BottomBar() {
           </button>
         </div>
         <NavItem
-          href="/Camera"
+          href="/Reels"
           icon={
             <svg
               width="30px"
@@ -565,24 +565,31 @@ export default function BottomBar() {
                 strokeLinejoin="round"
               ></g>
               <g id="SVGRepo_iconCarrier">
-                {" "}
-                <circle
-                  cx="12"
-                  cy="12"
-                  r="4"
-                  stroke="currentColor"
-                  strokeWidth="1.5"
-                ></circle>{" "}
                 <path
-                  d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8"
+                  d="M12 2L13.09 8.26L20 9L13.09 9.74L12 16L10.91 9.74L4 9L10.91 8.26L12 2Z"
                   stroke="currentColor"
                   strokeWidth="1.5"
                   strokeLinecap="round"
-                ></path>{" "}
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M19 15L19.5 16.5L21 17L19.5 17.5L19 19L18.5 17.5L17 17L18.5 16.5L19 15Z"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M5 15L5.5 16.5L7 17L5.5 17.5L5 19L4.5 17.5L3 17L4.5 16.5L5 15Z"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
               </g>
             </svg>
           }
-          label="Camera"
+          label="Reels"
         />
         <NavItem
           href="/Myprofile"
