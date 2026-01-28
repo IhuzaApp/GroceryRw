5fb607439ac22fdbd3f9db51e0167489c15c9b03
Abeltony-prog <abeltony03@gmail.com>
2026-01-28 16:02:06 +0200

    recepi view design: mboile

---
+++ src/components/Recipes/types.ts
@@ -0,0 +1,13 @@
+export interface MealDetail {
+  idMeal: string;
+  strMeal: string;
+  strCategory: string;
+  strArea: string;
+  strInstructions: string;
+  strMealThumb: string;
+  strTags: string | null;
+  strYoutube: string | null;
+  // For dynamic ingredient and measure properties
+  [key: string]: string | null;
+}
+
